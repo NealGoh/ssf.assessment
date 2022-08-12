5eec0aed722b6079c9490883bbe65b7edd0af34f
Neal Goh <nealgoh1996@gmail.com>
2022-08-12 12:53:49 +0800

    working on index.html

---
+++ src/main/java/ssf/crypto/assessment/controller/NewsController.java
@@ -0,0 +1,30 @@
+package ssf.crypto.assessment.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ssf.crypto.assessment.model.Articles;
+import ssf.crypto.assessment.service.NewsService;
+
+@Controller
+@RequestMapping("/articles")
+public class NewsController {
+
+    @Autowired
+    private NewsService newsSvc;
+
+    @GetMapping
+    public String getArticles(Model model, @RequestParam String news) {
+        List<Articles> articles = newsSvc.getArticles(news);
+        model.addAttribute("news", news); 
+        model.addAttribute("articles", articles);
+        return "articles";
+    }
+    
+}
